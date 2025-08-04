bc147ebc4cacd6e466e2316c5a11bc78656e8c90
LouisMartinez20 <218803341+LouisMartinez20@users.noreply.github.com>
2025-08-04 14:28:39 -0500

    feat: ejercicios de Functions

---
+++ JAVASCRIPT/Functions/Minimum.js
@@ -0,0 +1,9 @@
+function min(a,b){
+    if (a < b) { // si a es menor que b
+        return a;// devuelve a
+    } else {
+        return b;// devuelve b
+    }
+}
+console.log(min(5, 10)); // resultado: 5
+console.log(min(10, 30)); // resultado: 10
