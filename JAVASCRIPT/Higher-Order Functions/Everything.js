aec57adec12ffb280f6a29fcc863252fa836bef9
LouisMartinez20 <218803341+LouisMartinez20@users.noreply.github.com>
2025-08-11 11:55:29 -0500

    fix: cambio de la doble negacion de Everything.js

--- JAVASCRIPT/Higher-Order Functions/Everything.js
+++ JAVASCRIPT/Higher-Order Functions/Everything.js
@@ -1,5 +1,8 @@
 function every(array, test) {
-  return !array.some(element => !test(element));
+  for (const element of array) {
+    if (!test(element)) return false; 
+  }
+  return true; 
 }
 console.log(every([1, 3, 5], n => n < 10));
 // → true
