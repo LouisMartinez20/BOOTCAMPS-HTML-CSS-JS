81c34ed084bc978cb05f8cbb06416183ca9c9202
LouisMartinez20 <218803341+LouisMartinez20@users.noreply.github.com>
2025-08-18 17:00:13 -0500

    feat: segunda retroalimentacion

--- JAVASCRIPT/Higher-Order Functions/Everything.js
+++ JAVASCRIPT/Higher-Order Functions/Everything.js
@@ -6,8 +6,9 @@ function every(array, test) {
   return true; 
 }
 function every2(array, test) {
-  return !array.some(element => !test(element));// Usa some para verificar si hay algún elemento que no pasa el test
+  const someMeet = array.some(element => !test(element));
+  return !someMeet;// Usa some para verificar si hay algún elemento que no pasa el test
 }
 console.log(every([1, 3, 5], n => n < 10)); // → true
 console.log(every([2, 4, 16], n => n < 10)); // → false
-console.log(every([], n => n < 10)); // → true
+console.log(every2([], n => n < 10)); // → true
