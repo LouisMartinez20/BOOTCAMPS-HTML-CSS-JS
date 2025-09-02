b3d2652113d3956a9b3198bcccaded300d7da62b
LouisMartinez20 <218803341+LouisMartinez20@users.noreply.github.com>
2025-09-02 15:46:01 -0500

    refactor: mejorar responsive y eliminar archivos obsoletos

--- REACT/src/App.jsx
+++ REACT/src/App.jsx
@@ -1,13 +1,9 @@
-import './App.css'
 import { GoogleInReact } from './componets/GoogleInReact/GoogleInReact'
-
 function App() {
-
   return (
     <>
    <GoogleInReact />
     </>
   )
 }
-
 export default App
